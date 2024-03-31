299fe4e801cb225ad32cde67348f93c5fb9ead0b
Egor Kulikov <benstiller68@gmail.com>
2024-04-01 00:23:19 +0300

    chore(test): upadte test

--- __tests__/page-extractor.ts
+++ __tests__/page-extractor.ts
@@ -3,54 +3,54 @@ import pages from '../__mocks__/pages';
 import api from '../__mocks__/api';
 
 test('default offset fetch', async () => {
-  const chunkSize = 10;
+  const pageSize = 10;
   const data = await pageExtractor<number, number>(
     (_, offset) => offset,
     (offset) => {
-      const { data, total } = api(offset, chunkSize);
+      const { data, total } = api(offset, pageSize);
       return {
         data,
         total,
       };
     },
     {
-      chunkSize,
+      pageSize,
     }
   );
   expect(data.length).toBe(pages.length)
 });
 
 test('chunk smaller then total', async () => {
-  const chunkSize = 101;
+  const pageSize = 101;
   const data = await pageExtractor(
     (_, offset) => offset,
     (offset) => {
-      const { data, total } = api(offset, chunkSize);
+      const { data, total } = api(offset, pageSize);
       return {
         data,
         total,
       };
     },
     {
-      chunkSize,
+      pageSize,
     }
   );
   expect(data.length).toBe(pages.length)
 });
 
 test('order required', async () => {
-  const chunkSize = 10;
+  const pageSize = 10;
   const data = await pageExtractor(
     (_, offset) => offset,
     (offset) => {
-      const { data, total } = api(offset, chunkSize);
+      const { data, total } = api(offset, pageSize);
       return {
         data,
         total,
       };
     },
     {
-      chunkSize,
+      pageSize,
       orderRequired: true,
     }
   );
@@ -58,39 +58,39 @@ test('order required', async () => {
 });
 
 test('limit total', async () => {
-  const chunkSize = 10;
+  const pageSize = 10;
   const data = await pageExtractor(
     (_, offset) => offset,
     (offset) => {
-      const { data, total } = api(offset, chunkSize);
+      const { data, total } = api(offset, pageSize);
       return {
         data,
         total,
       };
     },
     {
-      chunkSize,
-      limitTotal: 10,
+      pageSize,
+      totalLimit: 10,
     }
   );
-  expect(data).toEqual(pages.slice(0, chunkSize));
+  expect(data).toEqual(pages.slice(0, pageSize));
 });
 
-test('chunk total', async () => {
-  const chunkSize = 10;
+test('page limit', async () => {
+  const pageSize = 10;
   const data = await pageExtractor(
     (_, offset) => offset,
     (offset) => {
-      const { data, total } = api(offset, chunkSize);
+      const { data, total } = api(offset, pageSize);
       return {
         data,
         total,
       };
     },
     {
-      chunkSize: 10,
-      limitChunks: 2,
+      pageSize,
+      pageLimit: 2,
     }
   );
-  expect(data).toEqual(pages.slice(0, chunkSize * 2));
+  expect(data).toEqual(pages.slice(0, pageSize * 2));
 });
